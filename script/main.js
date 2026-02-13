5cee9c8eb89bf015f29c3261d54dc586d520bdf5
rbdev09 <134378886+rbdev09@users.noreply.github.com>
2026-02-14 02:49:54 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -8,21 +8,17 @@ window.addEventListener('load', () => {
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes',
         cancelButtonText: 'No',
-    didOpen: () => {
-  const confirmBtn = Swal.getConfirmButton();
-  const cancelBtn = Swal.getCancelButton();
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.song').play();
+            animationTimeline();
+        } else {
+            animationTimeline();
+        }
+    });
+});
 
-  confirmBtn.addEventListener("click", () => {
-    const audio = document.querySelector(".song"); // your audio class
-    audio.muted = false;
-    audio.play().catch(e => console.log(e));
-    animationTimeline();
-  });
 
-  cancelBtn.addEventListener("click", () => {
-    animationTimeline();
-  });
-}
 // animation timeline
 const animationTimeline = () => {
     // split chars that needs to be animated individually
